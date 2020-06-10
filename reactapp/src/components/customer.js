0cc2525fd58825b54693a0936c0e7f7f030ab43d
Ubuntu <ubuntu@ip-172-31-5-93.ap-northeast-1.compute.internal>
2020-06-10 07:10:02 +0000

    add2

---
+++ reactapp/src/components/customer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Customer(){
+    return(
+        <div>
+            <p>홍길동</p>
+            <p>egoing</p>
+            <p>0101234567</p>
+            <p>잠실여고</p>
+        </div>
+    )
+}
+
+export default Customer;
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Customer from './components/Customer';
 import './index.css';
 import App from './App';
+import {Router, Route} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function(){
+  return(
+    <div>
+      <Customer />
+    </div>
+  )
+}
+
+ReactDOM.render(<App /> document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
+
