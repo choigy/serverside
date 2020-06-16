c0623885a7d44a2421366797cee283c15c636794
Ubuntu <ubuntu@ip-172-31-1-237.ap-northeast-1.compute.internal>
2020-06-16 08:53:20 +0000

    add9

--- serverapp.js
+++ serverapp.js
@@ -2,16 +2,23 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-var conn = mysql.CreateConnection({
-    host: localhost,
-    user: ubuntu,
-    password: roqkfwk77,
-    database: o2
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'ubuntu',
+    password: 'roqkfwk77',
+    database: 'o2'
 });
 conn.connect();
 
-app.use(bodyParser.urlencodedls({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.get('/students', function(req,res){
+    var sql = 'select * from customer';
+    conn.query(sql, function(err, rows, field){
+        res.send(rows);
+    })
+})
 
 app.get('/app/hello', function(req, res){
     res.send('hello');
