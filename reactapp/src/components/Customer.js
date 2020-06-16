ae5cd81b9f37c2d44655f5c995f3ab4319674a70
Ubuntu <ubuntu@ip-172-31-1-237.ap-northeast-1.compute.internal>
2020-06-16 06:17:25 +0000

    add8

--- reactapp/src/components/Customer.js
+++ reactapp/src/components/Customer.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Customer(){
     return(
         <div>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+            <p>홍길동</p>
+            <p>이경영</p>
+            <p>010-234-1112</p>
+            <p>잠실여고 </p>
         </div>
     );
 }
