18a6beebd0ced3d8e534ebb84227bb64d6c09955
Ubuntu <ubuntu@ip-172-31-1-237.ap-northeast-1.compute.internal>
2020-06-16 06:03:04 +0000

    add8

---
+++ reactapp/src/components/Customer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Customer(){
+    return(
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    );
+}
+
+export default Customer;
